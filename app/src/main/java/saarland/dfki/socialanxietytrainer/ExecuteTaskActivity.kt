f8288a512b37158ee22dd2bdd8861266e04df6d9
Timo Gühring <timo.guehring@googlemail.com>
2018-11-12 16:22:56 +0100

    Very basic UI skeleton

---
+++ app/src/main/java/saarland/dfki/socialanxietytrainer/ExecuteTaskActivity.kt
@@ -0,0 +1,12 @@
+package saarland.dfki.socialanxietytrainer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ExecuteTaskActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_execute_task)
+    }
+}
